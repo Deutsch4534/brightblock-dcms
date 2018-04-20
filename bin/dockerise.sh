54ae9fd4b85749e5f56673c93a39351b2740c385
Mike Cohen <mjoecohen@gmail.com>
2018-04-20 12:35:08 +0100

    deployment cahnges

--- bin/dockerise.sh
+++ bin/dockerise.sh
@@ -17,10 +17,13 @@ printf "\t5. Jenkins tags and pushes the container.\n";
 printf "\t6. Jenkins ssh to the target server and pulls the image.\n\n";
 
 DOCKER_COMPOSE_CMD='sudo /usr/local/bin/docker-compose'
+DOCKER_CMD='sudo /usr/bin/docker'
 DOCKER_HOME=/var/jenkins_home/brightblock/brightblock-docker/brightblock
 ls -lt dist
 cp -r dist/* $DOCKER_HOME/nginx/www/brightblock-dcms
 pushd $DOCKER_HOME > /dev/null
+
+$DOCKER_CMD ps -a
 $DOCKER_COMPOSE_CMD build
 
 printf "Finished brightblock-dcms nginx build and deployment.\n"
