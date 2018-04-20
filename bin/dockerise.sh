03cfe41f4229bd6a35dba99711491d7b4cd774cb
Mike Cohen <mjoecohen@gmail.com>
2018-04-20 12:31:21 +0100

    deployment cahnges

--- bin/dockerise.sh
+++ bin/dockerise.sh
@@ -14,10 +14,16 @@ printf "\t2. Jenkins runs - npm run build (prior to this script).\n";
 printf "\t3. Jenkins pushes dist folder to brightblock-docker/brightblock/nginx.\n";
 printf "\t4. Jenkins rebuilds the nginx container.\n";
 printf "\t5. Jenkins tags and pushes the container.\n";
-printf "\t6. Jenkins ssh to the target server and pulls the image.\n";
+printf "\t6. Jenkins ssh to the target server and pulls the image.\n\n";
 
+DOCKER_COMPOSE_CMD='sudo /usr/local/bin/docker-compose'
+DOCKER_HOME=/var/jenkins_home/brightblock/brightblock-docker/brightblock
 ls -lt dist
-cp -r dist/* /var/jenkins_home/brightblock/brightblock-docker/brightblock/nginx/www/brightblock-dcms
+cp -r dist/* $DOCKER_HOME/nginx/www/brightblock-dcms
+pushd $DOCKER_HOME > /dev/null
+$DOCKER_COMPOSE_CMD build
 
+printf "Finished brightblock-dcms nginx build and deployment.\n"
+printf "-----------------------------------------------------------------------------------------------------\n";
 
-printf "\nFinished brightblock-dcms nginx build and deployment.\n\n"
+popd > /dev/null
