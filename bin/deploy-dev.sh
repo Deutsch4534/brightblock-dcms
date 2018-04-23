c88ba6b7ef2a66864494f9dfad9097ae1eaafd18
Mike Cohen <mjoecohen@gmail.com>
2018-04-23 13:21:25 +0100

    finishing deployment work

--- bin/deploy-dev.sh
+++ bin/deploy-dev.sh
@@ -2,14 +2,15 @@
 #
 ############################################################
 
-printf "\n\nBuild and deploy D-CMS component\n";
+printf "\n\nBuild a local snapshot of the brightblock-dcms project.\n";
 printf "\n-----------------------------------------------------------------------------------------------------\n";
 printf "\t1. Jenkins pulls code from git (prior to this script).\n";
 printf "\t2. Jenkins creates dist folder - npm run build (prior to this script).\n";
-printf "\t3. Jenkins pushes dist folder to brightblock-docker/brightblock/nginx.\n";
+printf "\t3. Jenkins copies dist folder to brightblock-docker/brightblock/nginx.\n";
 printf "\t4. Jenkins builds the nginx image.\n";
-printf "\t5. Jenkins tags and pushes the image.\n";
-printf "\t6. Jenkins ssh to the target server and pulls the image.\n\n";
+
+printf "Note: DOCKER_HOME=/var/jenkins_home/brightblock/brightblock-docker/brightblock\n";
+printf "This volume might contain uncommitted changes and so this job is for local testing only!\n\n\n";
 
 DOCKER_COMPOSE_CMD='sudo /usr/local/bin/docker-compose'
 DOCKER_ID_USER='mijoco'
@@ -21,10 +22,12 @@ cp -r dist/* $DOCKER_HOME/nginx/www/brightblock-dcms
 pushd $DOCKER_HOME > /dev/null
 
 $DOCKER_CMD ps -a
-$DOCKER_COMPOSE_CMD build
-$DOCKER_CMD tag brightblock_nginx  $DOCKER_ID_USER/brightblock_nginx
-$DOCKER_CMD push $DOCKER_ID_USER/brightblock_nginx
-$DOCKER_COMPOSE_CMD restart nginx
+$DOCKER_COMPOSE_CMD down | printf "\nContainers not up\n"
+$DOCKER_COMPOSE_CMD build nginx
+$DOCKER_COMPOSE_CMD up -d | printf "\nContainers not down\n"
+#$DOCKER_CMD tag brightblock_nginx  $DOCKER_ID_USER/brightblock_nginx
+#$DOCKER_CMD push $DOCKER_ID_USER/brightblock_nginx
+#$DOCKER_COMPOSE_CMD restart nginx
 
 popd > /dev/null
 
